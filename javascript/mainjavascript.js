e5ae3ca9989e66c7777487dfe6c548b91afc1167
idabille <60378712+idabille@users.noreply.github.com>
2020-04-09 13:18:01 +0200

    kan ikke finde billede

---
+++ javascript/mainjavascript.js
@@ -0,0 +1,15 @@
+var bg = new Image();
+bg.src = "baggrund.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
