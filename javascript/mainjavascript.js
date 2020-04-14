4293179c0b28347f728be08328177ae5a230777e
idabille <60378712+idabille@users.noreply.github.com>
2020-04-14 11:52:15 +0200

    tilføjet js

--- javascript/mainjavascript.js
+++ javascript/mainjavascript.js
@@ -1,4 +1,209 @@
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
 
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
+    var background = new Background();
+
+    function animate(){
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+        // Start drawing here
+        background.render();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(100,100,50,50);
+        // End drawing here
+        ctx.restore();
+    }
+    var animateInterval = setInterval(animate, 30);
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
+    var background = new Background();
+
+    function animate(){
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+        // Start drawing here
+        background.render();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(100,100,50,50);
+        // End drawing here
+        ctx.restore();
+    }
+    var animateInterval = setInterval(animate, 30);
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
+    var background = new Background();
+
+    function animate(){
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+        // Start drawing here
+        background.render();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(100,100,50,50);
+        // End drawing here
+        ctx.restore();
+    }
+    var animateInterval = setInterval(animate, 30);
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
+    var background = new Background();
+
+    function animate(){
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+        // Start drawing here
+        background.render();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(100,100,50,50);
+        // End drawing here
+        ctx.restore();
+    }
+    var animateInterval = setInterval(animate, 30);
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
+    var background = new Background();
+
+    function animate(){
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+        // Start drawing here
+        background.render();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(100,100,50,50);
+        // End drawing here
+        ctx.restore();
+    }
+    var animateInterval = setInterval(animate, 30);
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
+var bg = new Image();
+bg.src = "img/konfetti.png";
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+
+    function Background(){
+        this.x = 0, this.y = 0, this.w = bg.width, this.h = bg.height;
+        this.render = function(){
+            ctx.drawImage(bg, this.x--, 0);
+            if(this.x <= -499){
+                this.x = 0;
+            }
+        }
+    }
+    var background = new Background();
+
+    function animate(){
+        ctx.save();
+        ctx.clearRect(0, 0, cW, cH);
+        // Start drawing here
+        background.render();
+        ctx.fillStyle = "orange";
+        ctx.fillRect(100,100,50,50);
+        // End drawing here
+        ctx.restore();
+    }
+    var animateInterval = setInterval(animate, 30);
+}
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
 
 //-------- Alert-pop up ---------//
 
